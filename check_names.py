320e2aefd4594a6a6989b5a99832456857e8447b
Hardik Patel <hardikp12@gmail.com>
2017-12-02 15:39:50 -0500

    Adding DB file as well as scripts to fetch the domains

---
+++ check_names.py
@@ -0,0 +1,42 @@
+import sqlite3
+from time import sleep
+
+import boto3
+
+
+class CheckNames(object):
+    def __init__(self):
+        self.client = boto3.client('route53domains')
+
+    def get_status(self, name):
+        response = self.client.check_domain_availability(DomainName=name)
+        status = response['Availability']
+
+        if status == 'PENDING':
+            sleep(1)
+
+            response = self.client.check_domain_availability(DomainName=name)
+            status = response['Availability']
+
+        return status
+
+
+if __name__ == '__main__':
+    check_names = CheckNames()
+
+    # CREATE TABLE domains (name TEXT, com TEXT, io TEXT, co TEXT, ai TEXT)
+    conn = sqlite3.connect('domains.db')
+    c = conn.cursor()
+
+    # Fetch 10 null entries
+    c.execute('SELECT * FROM domains WHERE com is NULL LIMIT 10')
+    entries = c.fetchall()
+
+    for entry in entries:
+        name = entry[0]
+        status = check_names.get_status(name + '.com')
+
+        query = 'UPDATE domains SET com = "{}" WHERE name = "{}"'.format(status, name)
+        print(query)
+        c.execute(query)
+        conn.commit()
---
+++ generate_names.py
@@ -0,0 +1,54 @@
+import random
+import sqlite3
+import string
+
+vowels = list('aeiou')
+consonents = list(set(string.ascii_lowercase) - set(vowels))
+
+
+class GenerateNames(object):
+    def __init__(self, min_length=4, max_length=5):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def new_name(self):
+        name = 'a'  # First letter has to be a
+
+        name += random.choice(consonents)
+        name += random.choice(vowels)
+
+        length = random.randrange(self.min_length, self.max_length + 1)
+        for i in range(length - 3):
+            name += random.choice(string.ascii_lowercase)
+
+        return name
+
+    def get(self, count=100):
+        names = []
+        for i in range(count):
+            name = self.new_name()
+            while name in names:
+                name = self.new_name()
+
+            names.append(name)
+
+        return names
+
+
+if __name__ == '__main__':
+    names = GenerateNames().get()
+    print(names)
+
+    # CREATE TABLE domains (name TEXT, com TEXT, io TEXT, co TEXT, ai TEXT)
+    conn = sqlite3.connect('domains.db')
+    c = conn.cursor()
+
+    for name in names:
+        c.execute('SELECT * FROM domains WHERE name = "{}"'.format(name))
+        fetched_names = c.fetchall()
+        if len(fetched_names) > 0:
+            continue
+
+        # Insert
+        c.execute('INSERT INTO domains (name) VALUES ("{}")'.format(name))
+        conn.commit()
